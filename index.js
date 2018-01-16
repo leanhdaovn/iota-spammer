ff40f03ff19a8a7ec8dd6763d04cb4117b4a4bfb
Dao Le <leanhdaovn@gmail.com>
2018-01-17 01:39:34 +0800

    Index files

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Create IOTA instance directly with provider
-var iota = new IOTA({
-    'provider': 'http://localhost:14265'
-});
-
-// now you can start using all of the functions
-iota.api.getNodeInfo(function (error, success) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(success);
-    }
-});
-
-// you can also get the version
-iota.version
+var iotaObj = new IOTA({'provider': 'http://02.iota-node.tilthat.com:14265'});
+var spammer = new Spammer({iotaObj, curlObj: curl});
+spammer.initialize();
