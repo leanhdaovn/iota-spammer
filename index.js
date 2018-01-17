ae4416a2120130af7781a52c5e113b98ebc61653
Dao Le <leanhdaovn@gmail.com>
2018-01-17 18:33:24 +0800

    Init curl and do local pow

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var iotaObj = new IOTA({'provider': 'http://02.iota-node.tilthat.com:14265'});
-var spammer = new Spammer({iotaObj, curlObj: curl});
-spammer.initialize();
+var iota = new IOTA({ 'provider': 'http://01.iota-node.tilthat.com:14265'});
+
+curl.init();
+curl.overrideAttachToTangle(iota);
+
+var spammer = new Spammer({ iotaObj: iota, curlObj: curl});
+
+spammer.init().then(spammer.start);
