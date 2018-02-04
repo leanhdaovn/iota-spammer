12e4d2d89795dfafc86c056ed93666d08d1fd673
Dao Le <leanhdaovn@gmail.com>
2018-02-04 16:04:14 +0800

    Refactor Transaction class

--- js/transaction.js
+++ js/transaction.js
@@ -1,25 +1,26 @@
 function Transaction({iotaObj, curlObj, sendingSeed, receivingAddress}) {
   const DEPTH = 3;
   const MIN_WEIGHT_MAGNITUDE = 14;
-  const TAG = 'IOTASPAMTRX';
+  const TAG = '9999ANDYLPROMOTERTX';
+  const transfers = [{ address: receivingAddress, value: 0, tag: TAG }];
+
+  const generateStandardCallback = (resolve, reject) => (error, result) => {
+    if (error) {
+      reject(error)
+    } else {
+      resolve(address);
+    }
+  };
 
   const sendTransfer = (reference) => new Promise((resolve, reject) => {
-    const transfers = [{ address: receivingAddress, value: 0, tag: TAG }];
     const options = reference ? { reference } : {};
-
     iotaObj.api.sendTransfer(
-      sendingSeed,
-      DEPTH,
-      MIN_WEIGHT_MAGNITUDE,
-      transfers,
-      options,
-      (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      }
+      sendingSeed, 
+      DEPTH, 
+      MIN_WEIGHT_MAGNITUDE, 
+      transfers, 
+      options, 
+      generateStandardCallback(resolve, reject)
     );
   });
 
